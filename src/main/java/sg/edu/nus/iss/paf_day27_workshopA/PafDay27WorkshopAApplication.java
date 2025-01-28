d5ca8bcceddf7d281377fd0a65c51367d19bacb4
sarahschan <sarahschan@hotmail.com>
2025-01-28 14:01:29 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/paf_day27_workshopA/PafDay27WorkshopAApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf_day27_workshopA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafDay27WorkshopAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafDay27WorkshopAApplication.class, args);
+	}
+
+}
